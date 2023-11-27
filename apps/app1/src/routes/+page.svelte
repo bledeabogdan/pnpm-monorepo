b682002abb7d5546366ca433f9e95941ceb2f00c
bledeabogdan <114652903+bledeabogdan@users.noreply.github.com>
2023-11-27 11:43:07 +0200

    first commit

---
+++ apps/app1/src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  <pre>styles</pre>
+  <h2 class="sui--single">
+    try editing <strong>src/routes/+page.svelte</strong>
+  </h2>
+  <p>issue: not loading</p>
+
+  <pre>@specialdoom/styles</pre>
+  <h2 class="sui--single-2">
+    try editing <strong>src/routes/+page.svelte</strong>
+  </h2>
+  <p>
+    issue: loading, but error in file regarding that it's not finding the
+    stylesheet
+  </p>
+</section>
+
+<style lang="scss">
+  @use "styles" as sty1;
+  @use "@specialdoom/styles/scss/test" as sty2;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+  }
+
+  h1 {
+    width: 100%;
+  }
+
+  .welcome {
+    display: block;
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding: 0 0 calc(100% * 495 / 2048) 0;
+  }
+
+  .welcome img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    display: block;
+  }
+</style>
---
+++ apps/app1/svelte.config.js
@@ -0,0 +1,26 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess";
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: preprocess({
+		scss: {
+			includePaths: [join(__dirname, 'node_modules/@specialdoom/styles'), join(__dirname, 'node_modules/styles')],
+		}
+	}),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
+};
+
+export default config;
